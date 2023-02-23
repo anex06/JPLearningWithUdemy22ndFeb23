0ef0a22e2acaef725f1a648af81032fe6a19d483
Anup <hianup05@gmail.com>
2023-02-23 21:32:04 +0530

    Commit 7 Function lambda expression with return type

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -1,14 +1,10 @@
 package com.myjetpack.mykotlinmodule
 
 fun main() {
-    println(add(5,7))
-    println(catAge(7))
+  name("Anup")
 }
-// Function definition Using Lambda expression
-// val lambda name:(Parameter Type) -> return type = { parameterList -> functionBody}
-val add:(Int, Int) ->Int = { a,b -> a+b}
 
-//We can use it if using only one parameter like below
-val catAge: (Int) -> Int = {
-        it -> it * 7
+//Unit in kotlin is treated as void
+val name:(String) -> Unit={
+   print("Hello my name is $it")
 }
