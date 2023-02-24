50e86670f4dac1a9d71534ca43343bc211dd40b2
Anup <hianup05@gmail.com>
2023-02-24 21:01:02 +0530

    Commit Collection 3: Filter in list

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -1,26 +1,10 @@
 package com.myjetpack.mykotlinmodule
 fun main() {
-    //Set
-    val mySet = mutableSetOf(1,2,3,4)
-    //Set can hold only unique data item if I add same value
-    //then the value will not be added
-    mySet.add(3)//This value will not be added as it is not unique to the set
-    mySet.add(21)
-    println(mySet)
-
-    //Map
-    val secretMap= mapOf("key1" to 1, "key2" to 2, "key3" to 3)
-    println(secretMap)
-    println(secretMap.keys)
-    println(secretMap.values)
-    if ("key1" in secretMap) println("Yes key is available")
-    else println("Key is not available")
-    if (3 in secretMap.values) println("Yes value is available")
-
-    //Mutable map
-    val myMutableMap = mutableMapOf("A" to 12, "B" to 32, "C" to 40)
-    println(myMutableMap)
-    myMutableMap["D"] = 55
-    println("After adding in map $myMutableMap")
-
+ val myListOfNames = listOf("Ram", "Shyam", "Jodu", "Modhu", "Manasi")
+  val found = myListOfNames.filter {
+       // it=="Modhu"
+      //it.length > 2
+      it.startsWith("m", ignoreCase = true) && it.endsWith("i", ignoreCase = true)
+    }
+    println(found)
 }
