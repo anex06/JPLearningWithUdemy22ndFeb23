054771ef062643027a503f5f2eaa0d299d691421
Anup <hianup05@gmail.com>
2023-02-24 14:33:51 +0530

    Commit 8: Trailing lambda

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -1,6 +1,7 @@
 package com.myjetpack.mykotlinmodule
 fun main() {
     // If the last parameter is a function then don't need to add the function as a parameter
+    //Curly braces in trailing lambda is always bolder compare to the normal one
     enhancedMessage("Hello"){
         println(it)
         12
