7b85a18c16f5d1d0bf955f38db40694569fe5500
Anup <hianup05@gmail.com>
2023-02-24 20:25:16 +0530

    Commit Collection 2: Set, Map(Mutable & Immutable)

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -1,19 +1,26 @@
 package com.myjetpack.mykotlinmodule
 fun main() {
+    //Set
+    val mySet = mutableSetOf(1,2,3,4)
+    //Set can hold only unique data item if I add same value
+    //then the value will not be added
+    mySet.add(3)//This value will not be added as it is not unique to the set
+    mySet.add(21)
+    println(mySet)
 
-    val listOfName = listOf("Anup","Riz")
-    listOfName.forEach {
-        println("Name : $it")
-    }
+    //Map
+    val secretMap= mapOf("key1" to 1, "key2" to 2, "key3" to 3)
+    println(secretMap)
+    println(secretMap.keys)
+    println(secretMap.values)
+    if ("key1" in secretMap) println("Yes key is available")
+    else println("Key is not available")
+    if (3 in secretMap.values) println("Yes value is available")
 
-    val mutableList = mutableListOf(12, 16, 22, 40)
-    mutableList.add(2)
-    mutableList.forEach{
-        println("Integer : $it")
-    }
+    //Mutable map
+    val myMutableMap = mutableMapOf("A" to 12, "B" to 32, "C" to 40)
+    println(myMutableMap)
+    myMutableMap["D"] = 55
+    println("After adding in map $myMutableMap")
 
-    mutableList.removeAt(2)
-    mutableList.forEach{
-        println("New Integer list : $it")
-    }
 }
