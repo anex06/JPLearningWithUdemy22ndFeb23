5d3a99ad39381bd82ada62ea236e7c23a9c31662
Anup <hianup05@gmail.com>
2023-02-27 13:43:48 +0530

    Commit OOPS Concept 3: Constructor with/without default params

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -1,14 +1,17 @@
 package com.myjetpack.mykotlinmodule
+
+import java.awt.Color
+
 fun main() {
- val car= Car()
-    car.color="Blue"
-    println("Car color is ${car.color} and model is ${car.model}")
-    println(car.drive())
+ val car= Car(color = "Green", model = "LLLL")
+    val secondCar = Car(color = "Yellow", model = "Audi")
+    println("First Car color is ${car.color} and model is ${car.model}")
+    println("Second Car color is ${secondCar.color} and model is ${secondCar.model}")
+   // println(car.drive())
 }
 
-class Car{
-    var color: String = "Red"//Can be changed later like above
-    val model: String="XMDB"
+class Car(//Can be changed later like above
+    val color: String = "Black"/*Default value*/, val model: String = "BMW"/*Default value*/){
 
     fun drive() {
         println("Driving the car....")
