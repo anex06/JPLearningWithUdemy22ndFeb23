c76c045e47af5718102b66a7336c85d89de33ee3
Anup <hianup05@gmail.com>
2023-02-27 13:50:55 +0530

    Commit OOPS Concept 3: init block to initialize the value

--- MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
+++ MyKotlinModule/src/main/java/com/myjetpack/mykotlinmodule/MyClass.kt
@@ -6,13 +6,23 @@ fun main() {
  val car= Car(color = "Green", model = "LLLL")
     val secondCar = Car(color = "Yellow", model = "Audi")
     println("First Car color is ${car.color} and model is ${car.model}")
-    println("Second Car color is ${secondCar.color} and model is ${secondCar.model}")
-   // println(car.drive())
+    //println("Second Car color is ${secondCar.color} and model is ${secondCar.model}")
+   println(car.drive())
+    car.speed(80, 140)
 }
 
 class Car(//Can be changed later like above
-    val color: String = "Black"/*Default value*/, val model: String = "BMW"/*Default value*/){
+    var color: String = "Black"/*Default value*/, var model: String = "BMW"/*Default value*/){
 
+    //init to initialize the value
+    init {
+        color = "White"
+        model = "Mahindra"
+    }
+
+    fun speed(minSpeed: Int, maxSpeed: Int){
+        println("Min speed is $minSpeed and max speed is $maxSpeed")
+    }
     fun drive() {
         println("Driving the car....")
     }
